d132e6fef33fb68009de39833c11cbeb0e347a61
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-04-11 23:58:04 +0700

    ADD : `Added createPayment method to PaymentService with midtrans integration`

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Helpers\TransactionHelper;
+use Illuminate\Support\Facades\Auth;
 use App\Repositories\PricingRepositoryInterface;
 use App\Repositories\TransactionRepositoryInterface;
 
@@ -19,5 +21,49 @@ class PaymentService
         $this->midtransService = $midtransService;
         $this->pricingRepository = $pricingRepository;
         $this->transactionRepository = $transactionRepository;
+    }
+
+    public function createPayment(int $pricingId)
+    {
+        $user = Auth::user();
+
+        $pricing = $this->pricingRepository->findById($pricingId);
+
+        $tax = 0.11;
+        $totalTax = $pricing->price * $tax;
+        $grandTotal = $pricing->price + $totalTax;
+
+        $params = [
+            'transaction_details' => [
+                'order_id' => TransactionHelper::generateUniqueTrxId(),
+                'gross_amount' => (int) $grandTotal,
+            ],
+            'customer_details' => [
+                'first_name' => $user->name,
+                'email' => $user->email,
+                'phone' =>'0812342255677'
+            ],
+            'item_details' => [
+                [
+                    'id' => $pricing->id,
+                    'price' => (int) $pricing->price,
+                    'quantity' => 1,
+                    'name' => $pricing->name,
+                ],
+                [
+                    'id' => 'tax',
+                    'price' => (int) $totalTax,
+                    'quantity' => 1,
+                    'name' => 'PPN 11%',
+                ],
+            ],
+            'custom_fields' => [
+                'custom_field1' => $user->id,
+                'custom_field2' => $pricingId,
+            ],
+         ];
+
+
+         return $this->midtransService->createSnapToken($params);
     }
 }
