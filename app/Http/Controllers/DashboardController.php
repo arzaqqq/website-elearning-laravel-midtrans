fded9b9ad79c35cf1527e2440eea63de4eeb9e5e
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-04-12 17:46:53 +0700

    ADD : `Added transaction service and methods to DashboardController for user transactions and subscription details.`

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use App\Service\TransactionService;
 
 class DashboardController extends Controller
 {
-    //
+    protected $transactionService;
+
+    public function __construct(
+        TransactionService $transactionService
+    ) {
+        $this->transactionService = $transactionService;
+    }
+
+    public function subscriptions()
+    {
+        $transactions = $this->transactionService->getUserTransaction();
+        return view ('front.subscrioptions', compact('transactions'));
+    }
+
+    public function subscription_details(Transaction $transaction)
+    {
+        return view('front.subscription_details', compact('transaction'));
+    }
 }
